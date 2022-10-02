a80dd866210c6c131f70ff52d5ee7e1f5c42b8bb
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2022-10-02 15:05:39 +0000

    added admin table

--- lib/widgets/admin_orders.dart
+++ lib/widgets/admin_orders.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:edwom/models/user_data.dart';
+import 'package:edwom/product.dart';
+import 'package:edwom/services/pdf_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,58 +10,96 @@ import '../providers.dart';
 import 'empty_widget.dart';
 
 class AdminOrders extends ConsumerWidget {
-  const AdminOrders({ Key? key }) : super(key: key);
+  const AdminOrders({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Scaffold(
-      backgroundColor:const Color(0xFFE7E3E0),
-      appBar: AppBar(),
-       body: StreamBuilder<List<Order>>(
-          stream: ref.read(databaseProvider)!.getOrders(),
+      backgroundColor: const Color(0xFFE7E3E0),
+      appBar: AppBar(
+        actions: [],
+      ),
+      body: StreamBuilder<QuerySnapshot<Order>>(
+          stream: ref.read(databaseProvider)!.getAllOrders(),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active &&
-                snapshot.data != null) {
-              if (snapshot.data!.isEmpty) {
-                return const EmptyWidget();
-              }
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    final order = snapshot.data![index];
+            //   return ListView.builder(
+            //       itemCount: snapshot.data!.length,
+            //       itemBuilder: (context, index) {
+            //         final order = snapshot.data![index];
 
-                    final total = order.products
-                        .map(((e) => e.price))
-                        .reduce((value, element) => value + element);
+            //         final total = order.products
+            //             .map(((e) => e.price))
+            //             .reduce((value, element) => value + element);
 
-                    return Padding(
-                        padding: const EdgeInsets.all(8.5),
-                        child: ListTile(
-                          title: Text(
-                            order.products.map((e) => e.name).join(', '),
-                            style: const TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                          subtitle: Text(
-                            order.timestamp!.toDate().toString(),
-                            style: const TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                          trailing: Text(
-                            "\$" + total.toString(),
-                            style: const TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ));
-                  });
+            //         return Padding(
+            //             padding: const EdgeInsets.all(8.5),
+            //             child: ListTile(
+            //               title: Text(
+            //                 order.products.map((e) => e.name).join(', '),
+            //                 style: const TextStyle(
+            //                   color: Colors.black,
+            //                 ),
+            //               ),
+            //               subtitle: Text(
+            //                 order.timestamp!.toDate().toString(),
+            //                 style: const TextStyle(
+            //                   color: Colors.black,
+            //                 ),
+            //               ),
+            //               trailing: Text(
+            //                 "\$" + total.toString(),
+            //                 style: const TextStyle(
+            //                   color: Colors.black,
+            //                 ),
+            //               ),
+            //             ));
+            //       });
+            // }
+            // return const Center(child: CircularProgressIndicator());
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
             }
-            return const Center(child: CircularProgressIndicator());
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text('Error loading data'),
+              );
+            }
+            if (snapshot.hasData) {
+              final data = snapshot.data?.docs.map((e) => e.data()).toList();
+              return Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                        columns: const [
+                          DataColumn(label: Text('Email')),
+                          DataColumn(label: Text('Products')),
+                          DataColumn(label: Text('Time')),
+                        ],
+                        rows: data
+                                ?.map((e) => DataRow(cells: [
+                                      DataCell(Text(e.email ?? '')),
+                                      DataCell(Text(e.products.getStringRepr)),
+                                      DataCell(Text(e.createdAt.toString())),
+                                    ]))
+                                .toList() ??
+                            []),
+                  ),
+                  TextButton(
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        PdfService.instance.generatePdf(data!);
+                      },
+                      child: const Text('Generate Orders'))
+                ],
+              );
+            }
+            return Container();
           }),
-      
     );
   }
+}
+
+extension on List<Product> {
+  String get getStringRepr => this.map((e) => e.name).join(', ');
 }
